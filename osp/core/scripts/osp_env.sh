f8173cdd586c5384bf5ecd0c852f717bff47f961
Tim BOWMAN <puffy@netherlogic.com>
2010-11-26 13:42:05 -0500

    Deactivate all the show/seq/shot functionality.

--- osp/core/scripts/osp_env.sh
+++ osp/core/scripts/osp_env.sh
@@ -22,31 +22,33 @@ export PATH=$PATH:$OSP_HOME/local/scripts:$OSP_HOME/core/scripts
 export PYTHONPATH=$PYTHONPATH:$OSP_HOME/local/scripts:$OSP_HOME/core/scripts
 export NUKE_PATH=$NUKE_PATH:$OSP_HOME/local/nuke:$OSP_HOME/core/nuke
 
-# Remove old show-specific variables
-unset OSP_SHOW
-unset OSP_SHOW_DIR
-unset OSP_SEQ
-unset OSP_SHOT
+alias oenv="set|grep ^OSP"
 
-# Set show variables if requested
-if [ ${#1} != 0 ]
-then
-    echo "Show code: "$1
-    export OSP_SHOW_CODE=$1
-    $OSP_HOME/core/scripts/osp_shows.sh $OSP_SHOW_CODE
-fi
+## Remove old show-specific variables
+#unset OSP_SHOW
+#unset OSP_SHOW_DIR
+#unset OSP_SEQ
+#unset OSP_SHOT
 
-# Set sequence variable if requested
-if [ ${#2} != 0 ]
-then
-    echo "Seq: "$2
-fi
-
-# Set shot variable if requested
-if [ ${#3} != 0 ]
-then
-    echo "Shot: "$3
-fi
+## Set show variables if requested
+#if [ ${#1} != 0 ]
+#then
+#    echo "Show code: "$1
+#    export OSP_SHOW_CODE=$1
+#    $OSP_HOME/core/scripts/osp_shows.sh $OSP_SHOW_CODE
+#fi
+#
+## Set sequence variable if requested
+#if [ ${#2} != 0 ]
+#then
+#    echo "Seq: "$2
+#fi
+#
+## Set shot variable if requested
+#if [ ${#3} != 0 ]
+#then
+#    echo "Shot: "$3
+#fi
 
 # Source the local studio environment if it exists.
 if [ -e $OSP_HOME/local/scripts/osp_local_env.sh ]
