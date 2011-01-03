99a1cd8eb56483435935336c204a6b004f074f47
Tim BOWMAN <puffy@netherlogic.com>
2011-01-02 21:51:36 -0500

    Rework the names of the ospenv scripts. Apologies for any confusion.

---
+++ osp/core/ospenv/osp.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+#
+# osp.py
+#
+# Module to contain central stuff used in all the osp* scripts.
+#
+
+# Globals
+envars = [ 'OSP_JOB',
+           'OSP_JOB_CODE',
+           'OSP_JOB_NAME',
+           'OSP_SEQ',
+           'OSP_SEQ_NAME',
+           'OSP_SHOT',
+           'OSP_SHOT_NAME', ]
+
---
+++ osp/job_scripts/osp_show_config.py
@@ -0,0 +1,15 @@
+#! /usr/local/python
+#
+# osp_show_config.py
+#
+# This is the file that contains basic information that OSP knows about a show. It
+# lives in the "_osp" directory in the show's root.
+#
+
+jobcode = "CODE"
+jobname = "Sample_Job_Name"
+
+dailiesdir = ""
+finalsdir = ""
+
+pass
