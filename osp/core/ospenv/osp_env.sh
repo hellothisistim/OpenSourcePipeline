3f6aedc590dffd966ee87a040c032c0d7885ee82
Tim BOWMAN <puffy@netherlogic.com>
2011-01-03 23:24:03 -0500

    Add alias for ospreload.

--- osp/core/ospenv/osp_env.sh
+++ osp/core/ospenv/osp_env.sh
@@ -6,12 +6,12 @@ echo "Loading OSP core environment."
 
 #export OSP_VERBOSE=True
 
-## Stop if OSP_HOME is not set.
-#if [ -z "${OSP_HOME}" ]
-#then 
-#    echo "OSP_HOME not set. Cannot find OSP scripts."
-#    exit 1
-#fi
+# Stop if OSP_HOME is not set.
+if [ -z "${OSP_HOME}" ]
+then 
+    echo "OSP_HOME not set. Cannot find OSP scripts."
+    exit 1
+fi
 
 # Find OSP volumes
 unset OSP_VOLS
@@ -23,33 +23,7 @@ export PYTHONPATH=$PYTHONPATH:$OSP_HOME/local/scripts:$OSP_HOME/core/scripts
 export NUKE_PATH=$NUKE_PATH:$OSP_HOME/local/nuke:$OSP_HOME/core/nuke
 
 alias oenv="set|grep ^OSP"
-#alias show=". osp_shows.sh"
-
-## Remove old show-specific variables
-#unset OSP_SHOW
-#unset OSP_SHOW_DIR
-#unset OSP_SEQ
-#unset OSP_SHOT
-
-## Set show variables if requested
-#if [ ${#1} != 0 ]
-#then
-#    echo "Show code: "$1
-#    export OSP_SHOW_CODE=$1
-#    $OSP_HOME/core/scripts/osp_shows.sh $OSP_SHOW_CODE
-#fi
-#
-## Set sequence variable if requested
-#if [ ${#2} != 0 ]
-#then
-#    echo "Seq: "$2
-#fi
-#
-## Set shot variable if requested
-#if [ ${#3} != 0 ]
-#then
-#    echo "Shot: "$3
-#fi
+alias ospreload=". $OSP_HOME/core/ospenv/osp_env.sh"
 
 # Source the local studio environment if it exists.
 if [ -e $OSP_HOME/local/scripts/osp_local_env.sh ]
